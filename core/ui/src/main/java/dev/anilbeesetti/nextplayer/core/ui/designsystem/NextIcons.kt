5d28280e89ac1b193e4d8947cf8559a7bfcddf03
deepu2135 <deepu2135@users.noreply.github.com>
2026-07-16 20:45:53 +0530

    fix: Use Menu icon for Chapters

--- core/ui/src/main/java/dev/anilbeesetti/nextplayer/core/ui/designsystem/NextIcons.kt
+++ core/ui/src/main/java/dev/anilbeesetti/nextplayer/core/ui/designsystem/NextIcons.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.icons.automirrored.rounded.DriveFileMove
 import androidx.compose.material.icons.automirrored.rounded.VolumeUp
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.AppSettingsAlt
+import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material.icons.rounded.ArrowDownward
 import androidx.compose.material.icons.rounded.ArrowUpward
 import androidx.compose.material.icons.rounded.Audiotrack
@@ -168,5 +169,5 @@ object NextIcons {
     val Image = Icons.Rounded.Image
     val Frame = Icons.Rounded.FilterFrames
     val Lock = Icons.Rounded.Lock
-    val Chapters = androidx.compose.material.icons.rounded.FormatListNumbered
+    val Chapters = Icons.Rounded.Menu
 }
